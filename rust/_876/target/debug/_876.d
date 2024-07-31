d134e162cdd9d998f481ef521237d2a4776132fa
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-07-30 23:17:11 -0300

    rm target

--- rust/_876/target/debug/_876.d
+++
@@ -1,1 +0,0 @@
-/home/guilherme/Coding/programming-logic/rust/_876/target/debug/_876: /home/guilherme/Coding/programming-logic/rust/_876/src/main.rs
--- rust/_876/target/debug/deps/_876-21cd32c78127224d.d
+++
@@ -1,5 +0,0 @@
-/home/guilherme/Coding/programming-logic/rust/_876/target/debug/deps/_876-21cd32c78127224d: src/main.rs
-
-/home/guilherme/Coding/programming-logic/rust/_876/target/debug/deps/_876-21cd32c78127224d.d: src/main.rs
-
-src/main.rs:
