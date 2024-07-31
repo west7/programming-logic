8953c942c7a3c1474dba182ed44b1f4508406d26
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-07-30 23:12:53 -0300

    add gitignore

--- rust/_876/target/debug/deps/_876-0fb40e34ec73cf62.d
+++
@@ -1,5 +0,0 @@
-/home/guilherme/Coding/programming-logic/rust/_876/target/debug/deps/lib_876-0fb40e34ec73cf62.rmeta: src/main.rs
-
-/home/guilherme/Coding/programming-logic/rust/_876/target/debug/deps/_876-0fb40e34ec73cf62.d: src/main.rs
-
-src/main.rs:
