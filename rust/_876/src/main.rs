16f3483c2e74febb6380ba450f548b05b9a105a1
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-09-07 21:06:16 -0300

    fix

--- rust/_876/src/main.rs
+++ rust/_876/src/main.rs
@@ -1,7 +1,6 @@
 // 876. Middle of the Linked List
 // link: https://leetcode.com/problems/middle-of-the-linked-list/?envType=daily-question&envId=2024-07-21
 
-
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
@@ -17,27 +16,36 @@ impl ListNode {
         }
     }
 }
+struct Solution;
 
-fn main(){
-}
+impl Solution {
+    fn _middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut current = &head;
+        let mut count = 0;
 
-fn _middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut current = &head;
-    let mut count = 0;
-
-    while let Some(node) = current {
-        count += 1;
-        current = &node.next;
-    }
-
-    current = &head;
-    let mid = count / 2;
-
-    for _ in 0..mid {
-        if let Some(node) = current {
+        while let Some(node) = current {
+            count += 1;
             current = &node.next;
         }
+
+        current = &head;
+        let mid = count / 2;
+
+        for _ in 0..mid {
+            if let Some(node) = current {
+                current = &node.next;
+            }
+        }
+        
+        current.clone()
     }
-    
-    current.clone()
+}
+fn main(){
+    let mut  head = Some(Box::new(ListNode::_new(1)));
+    head.as_mut().unwrap().next = Some(Box::new(ListNode::_new(2)));
+    head.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::_new(3)));
+    head.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::_new(4)));
+    head.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::_new(5)));
+
+    println!("{:?}", Solution::_middle_node(head));
 }
